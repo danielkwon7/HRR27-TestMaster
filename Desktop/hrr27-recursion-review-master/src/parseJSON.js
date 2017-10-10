f1bf655a4e6d2cb69bd9a92e1918a68bdf4a21fa
Daniel <dhkwon@andrew.cmu.edu>
2017-10-09 22:59:34 -0400

    Complete portion of parseJSON

--- Desktop/hrr27-recursion-review-master/src/parseJSON.js
+++ Desktop/hrr27-recursion-review-master/src/parseJSON.js
@@ -3,5 +3,22 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
-};
+  // "'ben'" -> 'ben'// your code goes here
+  if (json.split('')[0] === '\'') {
+    return json.slice(1,json.length-1);
+  // number
+  } else if (Number.parseInt(json)){
+    return Number.parseInt(json);
+  // null
+  } else if (json === 'null') {
+    return null;
+  } else if (json === undefined) {
+    return undefined;
+  } else if (json.split(''[0] === '[ ') {
+    var clean = json.split('').filter(function(x) {
+      return x !== '[' || x !== ']';
+    })
+    return clean.split(',');
+  }
+}
+
